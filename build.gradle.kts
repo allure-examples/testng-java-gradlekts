80163edbb226ccbb76b78847557f5ea69f5cc493
qameta-ci <qameta-ci@qameta.io>
2024-11-06 16:53:27 +0000

    update 2024-11-06T16:53:27

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
 }
 
 tasks.withType(Wrapper::class) {
-    gradleVersion = "8.7"
+    gradleVersion = "8.10.2"
 }
 
 group = "com.example.testng"
 version = "1.0-SNAPSHOT"
 
-val allureVersion = "2.26.0"
+val allureVersion = "2.29.0"
 val aspectJVersion = "1.9.22"
-val kotlinVersion = "1.9.23"
+val kotlinVersion = "1.9.25"
 
 java {
     toolchain {
@@ -39,12 +39,12 @@ tasks.test {
 dependencies {
     agent("org.aspectj:aspectjweaver:$aspectJVersion")
     
-    testImplementation("org.testng:testng:7.9.0")
+    testImplementation("org.testng:testng:7.10.2")
     
     testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
     testImplementation("io.qameta.allure:allure-testng")
 
-    testImplementation("org.slf4j:slf4j-simple:2.0.12")
+    testImplementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 repositories {
